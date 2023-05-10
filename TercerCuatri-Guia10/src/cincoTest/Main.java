aa8c0566606410a6f253f8d118ef2385cb6bd456
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 22:19:55 -0300

    Clase Main ej5
    Enunciado

---
+++ TercerCuatri-Guia10/src/cincoTest/Main.java
@@ -0,0 +1,10 @@
+package cincoTest;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*Crear un programa que pida al usuario que ingrese una lista de números separados por comas y los sume.
+        Manejar la excepción si alguno de los valores ingresados no es un número.*/
+
+    }
+}
