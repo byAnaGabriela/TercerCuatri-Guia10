a184977131be5bc6779acafe3d02b9202b340ca0
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 22:18:28 -0300

    Clase Main ej11
    Enunciado

---
+++ TercerCuatri-Guia10/src/onceTest/Main.java
@@ -0,0 +1,10 @@
+package onceTest;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*Crear un programa que lea un archivo de texto y lo imprima en la consola. Manejar
+        la excepción si se produce un error de lecto-escritura (e/s) durante la lectura del archivo.*/
+
+    }
+}
