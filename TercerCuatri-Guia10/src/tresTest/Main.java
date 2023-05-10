1b2e2fcf67f039006859551ba90ce9ed97a1a5aa
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 22:17:40 -0300

    Clase Main ej3
    Enunciado

---
+++ TercerCuatri-Guia10/src/tresTest/Main.java
@@ -0,0 +1,10 @@
+package tresTest;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*Crear un programa que lea un archivo de texto y cuente la cantidad de líneas que contiene.
+        Manejar la excepción si el archivo no existe.*/
+
+    }
+}
