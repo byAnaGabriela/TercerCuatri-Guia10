11b5b18894289866b2e8a158ba4c2863af9b4914
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 22:19:03 -0300

    Clase Main ej2
    Enunciado

---
+++ TercerCuatri-Guia10/src/dosTest/Main.java
@@ -0,0 +1,10 @@
+package dosTest;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*Crear una clase "Persona" con los atributos "nombre" y "edad". Crear un método "validarEdad" que lance una
+        excepción si la edad es menor que cero o mayor que 120.*/
+
+    }
+}
