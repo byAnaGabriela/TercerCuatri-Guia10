46e08d5f6f67f57c0548c697682719bfc9a5e079
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 22:18:51 -0300

    Clase Main ej9
    Enunciado

---
+++ TercerCuatri-Guia10/src/nueveTest/Main.java
@@ -0,0 +1,10 @@
+package nueveTest;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*Crear un programa que pida al usuario que ingrese un número y calcule su raíz cuadrada.
+        Manejar la excepción si el número ingresado es negativo.*/
+
+    }
+}
