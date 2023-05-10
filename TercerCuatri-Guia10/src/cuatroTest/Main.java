02a102f88a196af4fe2a0219901cc19380ef8211
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 22:19:29 -0300

    Clase Main ej4
    Enunciado

---
+++ TercerCuatri-Guia10/src/cuatroTest/Main.java
@@ -0,0 +1,10 @@
+package cuatroTest;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*Crear un programa que pida al usuario que ingrese una fecha en formato "dd/mm/aaaa" y la convierta a
+        un objeto del tipo "LocalDate". Manejar la excepción si la fecha ingresada no tiene el formato correcto.*/
+
+    }
+}
