2c42b43ee647e5dc9f2d5cc3efd8dbecf47a61b1
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 22:17:28 -0300

    Clase Main ej1
    Enunciado

---
+++ TercerCuatri-Guia10/src/unoTest/Main.java
@@ -0,0 +1,10 @@
+package unoTest;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*Crear un programa que pida al usuario que ingrese dos números enteros y realice la división entre ellos.
+        Manejar la excepción si el segundo número ingresado es cero.*/
+
+    }
+}
