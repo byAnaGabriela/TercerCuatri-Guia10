976d9e95e1124eee88bdea5eadd4421ec2683760
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 22:18:42 -0300

    Clase Main ej8
    Enunciado

---
+++ TercerCuatri-Guia10/src/ochoTest/Main.java
@@ -0,0 +1,10 @@
+package ochoTest;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*Crear un programa que lea un archivo de texto y cuente la cantidad de palabras que contiene.
+        Manejar la excepción si el archivo no existe. Para leer un archivo usar la clase File.*/
+
+    }
+}
