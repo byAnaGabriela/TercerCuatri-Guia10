09cebb31f7ae02d67c8e0ceb41c90118ffed3b2a
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 22:18:08 -0300

    Clase Main ej6
    Enunciado

---
+++ TercerCuatri-Guia10/src/seisTest/Main.java
@@ -0,0 +1,10 @@
+package seisTest;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*Crear una clase "CuentaBancaria" con un atributo "saldo". Crear un método "retirarDinero"
+        que lance una excepción si el saldo es insuficiente.*/
+
+    }
+}
