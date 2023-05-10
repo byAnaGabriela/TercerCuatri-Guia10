7e4bcaa02bb8f0d53749b021f0e92a5f0e68e951
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 22:19:18 -0300

    Clase Main ej10
    Enunciado

---
+++ TercerCuatri-Guia10/src/diezTest/Main.java
@@ -0,0 +1,10 @@
+package diezTest;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*Crear una clase "Estudiante" con los atributos "nombre" y "promedio". Crear un método "validarPromedio"
+        que lance una excepción si el promedio es menor que cero o mayor que diez.*/
+
+    }
+}
