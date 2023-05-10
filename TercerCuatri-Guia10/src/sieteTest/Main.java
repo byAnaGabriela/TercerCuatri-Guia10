1c908112854b5c38afc0cee2f0add01fdf4ce791
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 22:17:57 -0300

    Clase Main ej7
    Enunciado

---
+++ TercerCuatri-Guia10/src/sieteTest/Main.java
@@ -0,0 +1,10 @@
+package sieteTest;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*Crear una clase "SistemaTransitoOrdenado" con un atributo "saldo". Crear un método "retirarVehiculo"
+        que lance una excepción si el saldo es insuficiente.*/
+
+    }
+}
